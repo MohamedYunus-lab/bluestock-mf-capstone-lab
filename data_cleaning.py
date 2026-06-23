ec3d2117848b61104e0b04e58501851014abf99f
MOHAMED YUNUS ALI F <yunusalimohamed734@gmail.com>
2026-06-23 13:56:21 +0530

    data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,7 +4,6 @@ import os
 raw_path = "data/raw"
 processed_path = "data/processed"
 
-# Make sure processed folder exists
 os.makedirs(processed_path, exist_ok=True)
 
 for file in os.listdir(raw_path):
@@ -12,22 +11,18 @@ for file in os.listdir(raw_path):
         print(f"\n--- Cleaning {file} ---")
         df = pd.read_csv(os.path.join(raw_path, file))
 
-        # Step 2: Cleaning
-        # 1. Standardize column names
+        # Cleaning
         df.columns = df.columns.str.lower().str.replace(" ", "_")
 
-        # 2. Handle missing values (example: fill with empty string)
         df = df.fillna("")
 
-        # 3. Convert date columns if any
         for col in df.columns:
             if "date" in col:
                 try:
                     df[col] = pd.to_datetime(df[col])
                 except Exception:
-                    pass  # skip if not convertible
+                    pass  
 
-        # Save cleaned version
         cleaned_file = file.replace(".csv", "_clean.csv")
         df.to_csv(os.path.join(processed_path, cleaned_file), index=False)
 
